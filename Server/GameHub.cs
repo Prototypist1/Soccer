0a69de34363fd1eb7bfa25e76391cf05c9ba565b
Colin <ColinWielga@gmail.com>
2019-07-10 19:37:17 -0400

    whatever whatever

---
+++ Server/GameHub.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server
+{
+    public class GameHub : Hub
+    {
+        public void PlayerInputs(PlayerInputs playerInputs)
+        {
+        }
+
+        public void Positions(Positions positions)
+        {
+            Clients.All.SendAsync(nameof(Positions), positions);
+        }
+    }
+}
---
+++ Soccer/SignalRHandler.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soccer
+{
+    class SignalRHandler
+    {
+
+        public static async Task<SignalRHandler> Create() {
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:53353/ChatHub")
+                .Build();
+
+            connection.On<Positions>(nameof(Positions), x =>
+            {
+
+            });
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+
+            await connection.StartAsync();
+
+            return new SignalRHandler(connection);
+        }
+
+        HubConnection connection;
+        public SignalRHandler(HubConnection connection)
+        {
+            this.connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        private async void Send(PlayerInputs inputs)
+        {
+            await connection.InvokeAsync(nameof(PlayerInputs), inputs);   
+        }
+    }
+}
